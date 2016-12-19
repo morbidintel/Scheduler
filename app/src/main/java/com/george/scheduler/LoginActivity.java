f7ab43ea68f9d7250ec0f0bbcf5a06f5962b5d40
George Chew <hanged94@gmail.com>
2016-12-19 22:16:18 +0800

    Added to .gitignore Started Firebase integration

---
+++ app/src/main/java/com/george/scheduler/LoginActivity.java
@@ -0,0 +1,144 @@
+package com.george.scheduler;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity
+		extends AppCompatActivity
+		implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener
+{
+	private GoogleApiClient mGoogleApiClient;
+	private static final String TAG = "LoginActivity";
+	private static final int RC_SIGN_IN = 9001;
+
+	// Firebase instance variables
+	private FirebaseAuth mFirebaseAuth;
+	private FirebaseUser mFirebaseUser;
+	private String  mUsername, mPhotoUrl;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		// Assign fields
+		SignInButton mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+
+		// Set click listeners
+		mSignInButton.setOnClickListener(this);
+
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				.requestIdToken(getString(R.string.default_web_client_id))
+				.requestEmail()
+				.build();
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+				.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+				.build();
+
+		// Initialize FirebaseAuth
+		mFirebaseAuth = FirebaseAuth.getInstance();
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.sign_in_button:
+				Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+				startActivityForResult(signInIntent, RC_SIGN_IN);
+				break;
+		}
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+		if (requestCode == RC_SIGN_IN) {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if (result.isSuccess()) {
+				// Google Sign In was successful, authenticate with Firebase
+				GoogleSignInAccount account = result.getSignInAccount();
+				firebaseAuthWithGoogle(account);
+			} else {
+				// Google Sign In failed
+				Log.e(TAG, "Google Sign In failed.");
+			}
+		}
+	}
+
+	private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+		Log.d(TAG, "firebaseAuthWithGooogle:" + acct.getId());
+		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+		mFirebaseAuth.signInWithCredential(credential)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task) {
+						Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+						// If sign in fails, display a message to the user. If sign in succeeds
+						// the auth state listener will be notified and logic to handle the
+						// signed in user can be handled in the listener.
+						if (!task.isSuccessful()) {
+							Log.w(TAG, "signInWithCredential", task.getException());
+							Toast.makeText(LoginActivity.this, "Authentication failed.",
+									Toast.LENGTH_SHORT).show();
+						} else {
+							Toast.makeText(LoginActivity.this, "Welcome " + mFirebaseUser.getDisplayName() + "!", Toast.LENGTH_SHORT).show();
+							startActivity(new Intent(LoginActivity.this, MainActivity.class));
+							finish();
+						}
+					}
+				});
+	}
+
+	@Override
+	public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+		// An unresolvable error has occurred and Google APIs (including Sign-In) will not
+		// be available.
+		Log.d(TAG, "onConnectionFailed:" + connectionResult);
+		Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+	}
+}
+
+/*
+	// This is for logging out //
+	// Move to MainActivity, in the selection menu, in the future //
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                mUsername = ANONYMOUS;
+                startActivity(new Intent(this, LogInActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Checks if user is logged in //
+*/
--- app/src/main/java/com/george/scheduler/MainActivity.java
+++ app/src/main/java/com/george/scheduler/MainActivity.java
@@ -1,5 +1,6 @@
 package com.george.scheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,8 @@ import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import java.util.Calendar;
@@ -23,7 +26,12 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    MaterialCalendarView calendarView;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+	private String mUsername, mPhotoUrl;
+
+    private MaterialCalendarView calendarView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
